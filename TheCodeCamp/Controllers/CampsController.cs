8ea14c39c7de10a1a3ac5487e5156529e4e02292
georgecatalin <george.calin@gmail.com>
2020-01-31 15:11:28 +0200

    Creating an Action.

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public object Get()
+        {
+            return new { Name = "Victor", Type = "Controller" };
+        }
+    }
+}
