aba739e13d6a4d5b67a040cf860cd2b7bad6c6fc
georgecatalin <george.calin@gmail.com>
2020-02-03 14:34:18 +0200

    Return Related Data.

--- TheCodeCamp/Models/CampModel.cs
+++ TheCodeCamp/Models/CampModel.cs
@@ -11,5 +11,15 @@ namespace TheCodeCamp.Data
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+
+        //Associate related data to the model
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
