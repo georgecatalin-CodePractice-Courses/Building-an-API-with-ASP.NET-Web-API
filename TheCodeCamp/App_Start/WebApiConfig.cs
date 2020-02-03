3baa901e01984de5243391497dc097eddfefb402
georgecatalin <george.calin@gmail.com>
2020-02-03 13:23:52 +0200

    Contract Serialization in Web API

--- TheCodeCamp/App_Start/WebApiConfig.cs
+++ TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,25 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
 namespace TheCodeCamp
 {
-  public static class WebApiConfig
-  {
-    public static void Register(HttpConfiguration config)
+    public static class WebApiConfig
     {
-      // Web API configuration and services
-      AutofacConfig.Register();
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            AutofacConfig.Register();
 
-      // Web API routes
-      config.MapHttpAttributeRoutes();
+            //This is added for the contract serialization in the WebAPI as Javascript works better with CamelCase names in JSON Properties
+            //This actually changes the case to CamelCase instead of preserving the case of the underlying object
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-      config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
-      );
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
     }
-  }
 }
