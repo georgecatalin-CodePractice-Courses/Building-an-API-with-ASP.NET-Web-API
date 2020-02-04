7a24a4c22b02e3c100d2d46650fea2b9cc44d11d
georgecatalin <george.calin@gmail.com>
2020-02-04 15:53:24 +0200

    Creating a functional API.

---
+++ TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [HttpPost]
+        [Route("api/refreshSettings")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("appSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+        }
+    }
+}
